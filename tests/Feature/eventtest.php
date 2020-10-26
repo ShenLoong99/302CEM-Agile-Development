0456ee92f12d4c21eb00930039f490a295988e58
khaishiantan <khaishiantan@gmail.com>
2020-10-26 17:27:32 +0800

    Update eventtest.php

--- tests/Feature/eventtest.php
+++ tests/Feature/eventtest.php
@@ -9,7 +9,8 @@ class eventtest extends TestCase
 {
     public function test_load_registered_page()
     {
-        $response = $this->get('/e/registered_event/100');
+        // $response = $this->get('/e/registered_event/1');
+        $response = $this->json('GET','eventdetails/1',['id'=> 1]);
         $response->assertStatus(200);
     }
 }
